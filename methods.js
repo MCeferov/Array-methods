fc87954fc45a904730433f6112521a7b14cbe5e3
MCeferov <misomanxid@gmail.com>
2024-03-12 18:35:01 +0400

    10 Method

---
+++ methods.js
@@ -0,0 +1,93 @@
+// ! IndexOf()
+// let fruits = ["Apple", "Banana", "Orange", "Mango"];
+// let existent = fruits.indexOf("Orange");
+// let nonExistent = fruits.indexOf("Ananas");
+// console.log(existent);
+// console.log(nonExistent);
+
+//! LastIndexOf()
+
+// let fruits = ["Apple", "Banana", "Orange", "Mango", "Banana"];
+// let existent = fruits.lastIndexOf("Banana");
+// let nonExistent = fruits.lastIndexOf("Banana");
+//  console.log(existent);
+//  console.log(nonExistent);
+
+//! Concat()
+
+// let numbers = [1, 2, 3];
+// let allNumbers = array1.concat(4, 5, [6, 7]);
+// console.log(allNumbers);
+
+// let array1 = [1, 2, 3];
+// let array2 = [4, 5, 6];
+// let newArray = array1.concat(array2);
+// console.log(newArray);
+
+//! Join()
+
+// let fruits = ["Apple", "Banana", "Orange"];
+ // let result = fruits.join(", ");
+ // console.log(result);
+// console.log(fruits);
+
+// let numbers = [1, 2, 3, 4, 5];
+// let result = numbers.join("-");
+// console.log(numbers);
+// console.log(result);
+
+//! Filter()
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let evenNumbers = numbers.filter(function(number) {
+//     return number % 2 === 0;
+// });
+// console.log(evenNumbers);
+
+//! Every()
+
+// let numbers = [10, 20, 30, 40, 50];
+
+// let allBig = numbers.every(function(number) {
+//     return number > 5;
+// });
+
+// console.log(allBig); 
+
+//! Some()
+// let numbers = [10, 20, -5, 40, 50];
+
+// let hasNegative = numbers.some(function(number) {
+//     return number < 0;
+// });
+
+// console.log(hasNegative);
+
+//! Includes()
+
+// let fruits = ["Apple", "Banana", "Orange"];
+
+// let includesBanana = fruits.includes("Banana");
+// console.log(includesBanana); 
+
+// let includesGrape = fruits.includes("Grape");
+// console.log(includesGrape);
+
+//! Find()
+
+// let numbers = [1, 2, 3, 4, 5];
+
+// let found = numbers.find(function(number) {
+//     return number > 3;
+// });
+
+// console.log(found);
+
+// let numbers = [1, 2, 3, 4, 5];
+
+//! findIndex()
+
+// let foundIndex = numbers.findIndex(function(number) {
+//     return number > 3;
+// });
+
+// console.log(foundIndex); 
